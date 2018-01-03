bb7a38bd29a284363ba5b1d503090d748f76ff42
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-01-03 12:52:07 +0000

    Get specific version of pyke.

--- get_sources.sh
+++ get_sources.sh
@@ -9,8 +9,9 @@ git clone https://github.com/SciTools/cf_units.git
 git clone https://github.com/SciTools/iris.git
 
 wget ftp://ftp.unidata.ucar.edu/pub/udunits/udunits-2.2.25.tar.gz
-wget https://sourceforge.net/projects/pyke/files/latest/download
-wget --no-check-certificate https://sourceforge.net/projects/pyke/files/latest/download
+# wget https://sourceforge.net/projects/pyke/files/latest/download
+# wget --no-check-certificate https://sourceforge.net/projects/pyke/files/latest/download
+wget --no-check-certificate https://sourceforge.net/projects/pyke/files/pyke/1.1.1/pyke-1.1.1.zip/download -o pyke-1.1.1.zip
 
 wget https://pypi.python.org/packages/94/63/f54920c2ddbe3e1341a4c268f7091bf1bf53c3d84f4b115aa5beea64aef9/Cython-0.27.tar.gz#md5=3a26a5940da1e9f95d8776eabd888b07
 wget http://download.osgeo.org/geos/geos-3.5.1.tar.bz2
--- unzip_sources.sh
+++ unzip_sources.sh
@@ -1,3 +1,5 @@
+cd src
+
 unzip pyke-1.1.1.zip 
 
 tar xvf udunits-2.2.25.tar.gz 
@@ -5,3 +7,5 @@ tar xvf Cython-0.27.tar.gz
 tar xvf geos-3.5.1.tar.bz2 
 tar xvf six-1.11.0.tar.gz 
 tar xvf proj-4.9.3.tar.gz 
+
+cd ..
