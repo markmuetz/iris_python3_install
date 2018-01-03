f026c498c60b8883af155451427c9b0c0c2bb778
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-01-03 12:25:28 +0000

    Bash scripts for py3 installation of iris on ARCHER.

---
+++ clean_all.sh
@@ -0,0 +1,20 @@
+rm -rf /work/n02/n02/mmuetz/python3_libs/lib/python3.4/site-packages/
+rm -rf /work/n02/n02/mmuetz/python3_libs
+
+rm -rf biggus
+rm -rf cartopy
+rm -rf cf_units
+rm -rf Cython-0.27
+rm -rf Cython-0.27.tar.gz
+rm -rf geos-3.5.1
+rm -rf geos-3.5.1.tar.bz2
+rm -rf iris
+rm -rf proj-4.9.3
+rm -rf proj-4.9.3.tar.gz
+rm -rf pyke-1.1.1
+rm -rf pyke-1.1.1.zip
+rm -rf pyshp
+rm -rf six-1.11.0
+rm -rf six-1.11.0.tar.gz
+rm -rf udunits-2.2.25
+rm -rf udunits-2.2.25.tar.gz
---
+++ get_sources.sh
@@ -0,0 +1,18 @@
+git clone https://github.com/GeospatialPython/pyshp
+
+git clone https://github.com/SciTools/biggus.git
+git clone https://github.com/SciTools/cartopy.git
+git clone https://github.com/SciTools/cf_units.git
+git clone https://github.com/SciTools/iris.git
+
+wget ftp://ftp.unidata.ucar.edu/pub/udunits/udunits-2.2.25.tar.gz
+wget https://sourceforge.net/projects/pyke/files/latest/download
+wget --no-check-certificate https://sourceforge.net/projects/pyke/files/latest/download
+
+wget https://pypi.python.org/packages/94/63/f54920c2ddbe3e1341a4c268f7091bf1bf53c3d84f4b115aa5beea64aef9/Cython-0.27.tar.gz#md5=3a26a5940da1e9f95d8776eabd888b07
+wget http://download.osgeo.org/geos/geos-3.5.1.tar.bz2
+
+wget https://pypi.python.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz#md5=d12789f9baf7e9fb2524c0c64f1773f8
+wget http://download.osgeo.org/proj/proj-4.9.3.tar.gz
+
+
