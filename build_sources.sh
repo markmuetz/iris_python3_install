d8eed75b959ff43c6ca06f353ac47c2f8f682ef7
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-01-04 10:06:02 +0000

    Change to using setup.py --user to fix python build problem.

--- build_sources.sh
+++ build_sources.sh
@@ -1,3 +1,4 @@
+set -x
 if [ -z "$LIBDIR" ]; then
     echo "Need to set LIBDIR"
     exit 1
@@ -5,18 +6,22 @@ fi
 
 mkdir -p $LIBDIR/lib/python3.4/site-packages/
 export PATH=$PATH:$LIBDIR/usr/local/bin/
-export PYTHONPATH=$PYTHONPATH:$LIBDIR/lib/python3.4/site-packages/
+if [ -z "$PYTHONPATH" ]; then
+    export PYTHONPATH=$LIBDIR/lib/python3.4/site-packages/
+else
+    export PYTHONPATH=$PYTHONPATH:$LIBDIR/lib/python3.4/site-packages/
+fi
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBDIR/usr/local/lib
 export CFLAGS="-I$LIBDIR/usr/local/include"
 
 cd src/
 
 cd biggus/
-python3 setup.py install --prefix=$LIBDIR
+python3 setup.py install --user
 cd ..
 
 cd cf_units/
-python3 setup.py install --prefix=$LIBDIR
+python3 setup.py install --user
 cd ..
 
 cd udunits-2.2.25/
@@ -26,19 +31,19 @@ make install
 cd ..
 
 cd pyke-1.1.1/
-python3 setup.py install --prefix=$LIBDIR
+python3 setup.py install --user
 cd ..
 
 cd Cython-0.27/
-python3 setup.py install --prefix=$LIBDIR
+python3 setup.py install --user
 cd ..
 
 cd pyshp/
-python3 setup.py install --prefix=$LIBDIR
+python3 setup.py install --user
 cd ..
 
 cd six-1.11.0/
-python3 setup.py install --prefix=$LIBDIR
+python3 setup.py install --user
 cd ..
 
 cd geos-3.5.1/
@@ -55,12 +60,12 @@ cd ..
 
 cd cartopy/
 ldconfig
-python3 setup.py install --prefix=$LIBDIR
+python3 setup.py install --user
 cd ..
 
 cd iris/
 git checkout v1.13.0
-python3 setup.py install --prefix=$LIBDIR
+python3 setup.py install --user
 cd ..
 
 cd ..
